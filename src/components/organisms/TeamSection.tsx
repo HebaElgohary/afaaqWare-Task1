cce29ecf10c32c28337972824ece84371d16bee4
HebaElgohary <hebasamir535@gmail.com>
2026-03-29 16:29:38 +0200

    feat:add TeamSection

--- src/components/organisms/TeamSection.tsx
+++ src/components/organisms/TeamSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TeamSection() {
   return (
-    <div className='bg-rose-200 !p-5 flex justify-center items-center font-medium'>TeamSection</div>
+    <div className='bg-rose-300 !p-5 flex justify-center items-center font-medium'>TeamSection</div>
   )
 }
 
